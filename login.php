eb11e192a51973088c5a250a9834b5174b633308
Aldiosebastiaan <aldiosebastian9@gmail.com>
2024-12-31 00:07:09 +0700

    ubah judul form login

--- login.php
+++ login.php
@@ -49,7 +49,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <title>Login | My Schedule</title>
+    <title>Login | Welcome to My Schedule</title>
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
       rel="stylesheet"
